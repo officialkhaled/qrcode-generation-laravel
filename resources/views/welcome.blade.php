b50341664e55b0ffeaa015a4bc8a5f6877fb34a6
Khaled Hossain <khaledhossain778@gmail.com>
2025-02-04 21:29:02 +0600

    project initialization

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
 
     <title>QR Code Generator</title>
 
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
 
